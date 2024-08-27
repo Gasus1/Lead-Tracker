684e70b9cb89d1d859214f3716a0dc8304ac63fb
Gasus1 <gasus9697@gmail.com>
2024-08-27 08:29:36 +0300

    v1.1
    Created functionality for a button with JS

---
+++ js/index.js
@@ -0,0 +1,28 @@
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value.trim());
+    clearInput(inputEl);
+    focusOnInput(inputEl);
+    renderLeads();
+});
+
+function clearInput(target) {
+    target.value = "";
+}
+
+function focusOnInput(target) {
+    target.focus();
+}
+
+function renderLeads() {
+    let listItems = "";
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `<li><a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a></li>`;
+    }
+    ulEl.innerHTML = listItems;
+}
+
