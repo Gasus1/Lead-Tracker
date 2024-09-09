289ed811219bbfb92716427ab7099e451833bb3d
Gasus1 <gasus9697@gmail.com>
2024-09-09 12:17:03 +0300

    v1.2

--- js/index.js
+++ js/index.js
@@ -1,13 +1,38 @@
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 const ulEl = document.getElementById("ul-el");
+
+
+tabBtn.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render(myLeads);
+    });
+});
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
+}
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value.trim());
     clearInput(inputEl);
     focusOnInput(inputEl);
-    renderLeads();
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+});
+
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
 });
 
 function clearInput(target) {
@@ -18,10 +43,10 @@ function focusOnInput(target) {
     target.focus();
 }
 
-function renderLeads() {
+function render(leads) {
     let listItems = "";
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `<li><a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a></li>`;
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `<li><a href="${leads[i]}" target="_blank">${leads[i]}</a></li>`;
     }
     ulEl.innerHTML = listItems;
 }
